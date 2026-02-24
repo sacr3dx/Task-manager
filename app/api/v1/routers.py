4ea3f3606f20d5040e3895fbee91e97cbc36a135
Yaroslav <yaroslav@MacBook-Air-Yaroslav.local>
2026-02-24 14:09:02 +0300

    Create two methods with db

--- app/api/v1/routers.py
+++ app/api/v1/routers.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 from app.schemas.task_schema import TaskAddSchema
 from app.core.database import SessionDep, engine
-from app.model.base_model import TaskModel, Base
-
+from app.model.base_model import TaskModel, TaskResponse, Base
+from pathlib import Path
+from sqlalchemy import select
 
 router = APIRouter(prefix="/main_task", tags=["Main actions with tasks"])
 
@@ -18,16 +19,12 @@ async def add_new_task(data: TaskAddSchema, session: SessionDep):
     await session.commit()
     return {'message': 'New task added'}
 
-@router.post("/setup_database")
-async def create_db():
-    async with engine.connect() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
 
-
-@router.get('/')
-def get_tasks():
-    ...
+@router.get('/tasks', response_model=list[TaskResponse])
+async def get_tasks(session: SessionDep):
+    result = await session.execute(select(TaskModel))
+    tasks = result.scalars().all()
+    return tasks
 
 
 
@@ -36,8 +33,20 @@ def get_special_task():
     ...
 
 
-
-
 @router.delete('/{num_task}')
 def delete_task():
     ...
+
+
+@router.post("/setup_database")
+async def create_db():
+    db_path=Path('tasks.db')
+    if not db_path.exists():
+        async with engine.connect() as conn:
+            await conn.run_sync(Base.metadata.drop_all)
+            await conn.run_sync(Base.metadata.create_all)
+            return {"message": "Database has been created"}
+    else:
+         return {"message": "Database in the project"}
+
+
--- app/model/base_model.py
+++ app/model/base_model.py
@@ -1,3 +1,4 @@
+from pydantic import BaseModel
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from typing import Optional
 
@@ -11,3 +12,10 @@ class TaskModel(Base):
     title: Mapped[str]
     description: Mapped[Optional[str]]
     deadline: Mapped[Optional[int]]
+
+class TaskResponse(BaseModel):
+    id: int
+    title: str
+
+    class Config:
+        from_attributes=True
